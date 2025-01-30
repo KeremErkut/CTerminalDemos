0d9b7a0776dbf14db288ad6cbec5e1c2b16f488c
Kerem Erkut Çiftlikçi <76708644+KeremErkut@users.noreply.github.com>
2025-01-30 19:42:56 +0300

    Update demo2.c

--- demo2.c
+++ demo2.c
@@ -3,12 +3,12 @@
 
 int main(void) {
     for (int i = 0; i < 20; i++) {
-        printf("\033[H\033[J"); // Terminali temizle
+        printf("\033[H\033[J"); 
         for (int j = 0; j < i; j++) {
-            printf(" "); // Boşluk ekle
+            printf(" "); 
         }
         printf("\033[1;31m Kerem\033[0m\n");
-        usleep(100000); // 100ms bekle
+        usleep(100000); 
     }
 
     return 0;
